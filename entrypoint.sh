d7ca57e3d8619b67d684172a6401b47cb4367f53
Jan Piskvor Martinec <github@piskvor.org>
2018-03-26 15:00:35 +0200

    Add ssh-config if /ssh_config is found Enable host port Add extra options if provided Use batch mode Timestamp entries

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,35 +11,55 @@ if [ -f "${KNOWN_HOSTS}" ]; then
     STRICT_HOSTS_KEY_CHECKING=yes
 fi
 
+CONFIG_FILE=${SSH_CONFIG_FILE:=/ssh_config}
+if [ -f "${CONFIG_FILE}" ]; then
+    chmod 0400 ${CONFIG_FILE}
+    CONFIG_FILE_ARG="-F ${CONFIG_FILE}"
+fi
+
+if [ ! -z "${SSH_HOSTPORT:=}" ]; then
+    HOSTPORT_ARG="-p ${SSH_HOSTPORT}"
+fi
+
 # Pick a random port above 32768
 DEFAULT_PORT=$RANDOM
 let "DEFAULT_PORT += 32768"
 echo [INFO] Tunneling ${SSH_HOSTUSER:=root}@${SSH_HOSTNAME:=localhost}:${SSH_TUNNEL_REMOTE:=${DEFAULT_PORT}} to ${SSH_TUNNEL_HOST=localhost}:${SSH_TUNNEL_LOCAL:=22}
 
 echo autossh \
+ ${CONFIG_FILE_ARG:=} \
  -M 0 \
  -N \
  -o StrictHostKeyChecking=${STRICT_HOSTS_KEY_CHECKING} ${KNOWN_HOSTS_ARG:=} \
  -o ServerAliveInterval=5 \
  -o ServerAliveCountMax=1 \
+ -o IdentitiesOnly=yes \
+ -o BatchMode=yes \
+ -o VisualHostKey=yes \
  -t -t \
  -i ${SSH_KEY_FILE:=/id_rsa} \
  ${SSH_MODE:=-R} ${SSH_TUNNEL_REMOTE}:${SSH_TUNNEL_HOST}:${SSH_TUNNEL_LOCAL} \
- -p ${SSH_HOSTPORT:=22} \
+ ${HOSTPORT_ARG:=} \
+ ${SSH_EXTRA_OPTIONS:=} \
  ${SSH_HOSTUSER}@${SSH_HOSTNAME}
 
 AUTOSSH_PIDFILE=/autossh.pid \
 AUTOSSH_POLL=10 \
-AUTOSSH_LOGLEVEL=0 \
+AUTOSSH_LOGLEVEL=2 \
 AUTOSSH_LOGFILE=/dev/stdout \
 autossh \
+ ${CONFIG_FILE_ARG:=} \
  -M 0 \
  -N \
  -o StrictHostKeyChecking=${STRICT_HOSTS_KEY_CHECKING} ${KNOWN_HOSTS_ARG:=}  \
  -o ServerAliveInterval=5 \
  -o ServerAliveCountMax=1 \
+ -o IdentitiesOnly=yes \
+ -o BatchMode=yes \
+ -o VisualHostKey=yes \
  -t -t \
  -i ${SSH_KEY_FILE:=/id_rsa} \
  ${SSH_MODE:=-R} ${SSH_TUNNEL_REMOTE}:${SSH_TUNNEL_HOST}:${SSH_TUNNEL_LOCAL} \
- -p ${SSH_HOSTPORT:=22} \
- ${SSH_HOSTUSER}@${SSH_HOSTNAME}
+ ${HOSTPORT_ARG:=} \
+ ${SSH_EXTRA_OPTIONS:=} \
+ ${SSH_HOSTUSER}@${SSH_HOSTNAME} 2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
